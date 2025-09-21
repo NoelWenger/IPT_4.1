f95b4e2070b6388e261915d00b0615b04a891d2d
NoelWenger <Noel_Wenger@sluz.ch>
2025-09-21 21:15:57 +0200

    Vue-Projekt mit ToDo-Komponente initialisiert

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Todo from './components/Todo.vue'
+</script>
+
+<template>
+  <main>
+    <Todo />
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+}
+</style>
